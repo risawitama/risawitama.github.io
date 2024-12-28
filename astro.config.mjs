39fbcacd6fdde83a88b39fea88622402564bc160
Risa Witama <60031111+risawitama@users.noreply.github.com>
2024-12-29 00:19:41 +0700

    is up?

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://risa.my.id",
   trailingSlash: "always",
   integrations: [
     tailwind(
